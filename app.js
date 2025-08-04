4aa09559ab336af44492a4e3f441b20db375e32b
webexpert.esfera <developer@esferasoft.com>
2025-08-04 16:29:55 +0530

    changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3003;
 
 // Enable CORS for all routes
 app.use(cors());
